075115c238907e89d2169f434d2d3ebe36d460e9
Jake <jwilkins0519@gmail.com>
2021-03-25 14:48:54 +0000

    Updating pickle generation;adding daily returns calculation

--- generateDF.py
+++ generateDF.py
@@ -4,7 +4,12 @@ import pandas as pd
 filename = "./data/AMZN.csv"
 
 dates = []
+openList = []
+high = []
+low = []
+close = []
 adjusted_close = []
+volume = []
 
 with open(filename) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -15,10 +20,15 @@ with open(filename) as csv_file:
             line_count += 1
         else:
             dates.append(row[0])
+            openList.append(row[1])
+            high.append(row[2])
+            low.append(row[3])
+            close.append(row[4])
             adjusted_close.append(row[5])
+            volume.append(row[6])
             line_count += 1
 
-df = pd.DataFrame(list(zip(dates, adjusted_close)), columns=['Dates', 'Adj Close'])
+df = pd.DataFrame(list(zip(dates, openList, high, low, close, adjusted_close, volume)), columns=['Dates','Open', 'High', 'Low', 'Close', 'Adj Close', 'Volume'])
 
 print(df)
 
--- sectionOneAnal.py
+++ sectionOneAnal.py
@@ -2,4 +2,16 @@ import pandas as pd
 
 amazon = pd.read_pickle('./AMZN.pkl')
 
+def dailyReturns():
+    dailyReturns = []
+    for i in range(len(amazon)):
+        openingValue = float(amazon['Open'][i])
+        closingValue = float(amazon['Adj Close'][i])
+        dailyReturn = closingValue - openingValue
+        dailyReturns.append(dailyReturn)
+
+    amazon['Daily Return'] = dailyReturns
+
+dailyReturns()
+
 print(amazon)
